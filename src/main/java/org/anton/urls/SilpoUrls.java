3cef2a60219098383bf2bb87d308c8a84b2d1987
mccreed13 <a.lapushenko13@gmail.com>
2025-06-08 23:11:56 +0300

    Urls metro and silpo added

--- src/main/java/org/anton/urls/SilpoUrls.java
+++ src/main/java/org/anton/urls/SilpoUrls.java
@@ -6,12 +6,14 @@ public class SilpoUrls extends BaseUrls {
     public static final String url_kyrka = "https://silpo.ua/category/kuriatyna-4426";
     public static final String url_syhari_panirovochni = "https://silpo.ua/category/paniruvannia-4943";
     public static final String url_sil = "https://silpo.ua/category/sil-4901";
+    public static final String url_specii = "https://silpo.ua/category/spetsii-4941";
 
     public static List<String> getAllUrls() {
         return List.of(
                 url_kyrka,
                 url_syhari_panirovochni,
-                url_sil
+                url_sil,
+                url_specii
         );
     }
 }
